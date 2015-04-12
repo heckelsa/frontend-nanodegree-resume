c9a187f576df0a3255b2170f7d8819eef7aee2d1
Saskia Heckele <saskia.heckele@web.de>
2015-04-12 15:24:27 +0200

    Changes according to the ProjectDetails and added Style

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,6 @@
 /*
-
 This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
 Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
 Cameron Pittman
 */
 
@@ -13,21 +10,21 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span>%data%</span>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="green-text">%contact%</span><span class="black-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="green-text">mobile</span><span class="black-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="green-text">email</span><span class="black-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="green-text">twitter</span><span class="black-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="green-text">github</span><span class="black-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="green-text">blog</span><span class="black-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="green-text">location</span><span class="black-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><span class="black-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,21 +2,23 @@
 //############## DECLARATION ##################
 //=============================================
 
+var replaceIdentifier = "%data%";
+var loremIpsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
 // BIO
 var bio = {
 	"name" : "Saskia Heckele",
 	"role" : "Web Developer",
 	"contacts" : {
-		"mobile" : "089 99 89 999",
+		"mobile" : "+49 (0) 15774339156",
 		"email" : "saskia.heckele@pentasys.de",
 		"github" : "@heckelsa",
 		"twitter" : "heckelsa",
 		"location" : "Munich, Germany"
 	},
-	"welcomeMessage" : "Welcome to my Resume!",
-	"skills" : ["awesome", "more awesome", "hungry", "more hungry"],
-	"biopic" : "images/fry.jpg",
-	"display" : "FUNCTION"
+	"welcomeMessage" : "Welcome to my Resume! I want to show you what I've done in my live!",
+	"skills" : ["HTML/CSS", "JavaScript", "jQuery", "Photography", "Testing"],
+	"biopic" : "images/biopic.jpg"
 }
 
 
@@ -27,15 +29,23 @@ var education = {
 				"location" : "Furtwangen, Germany",
 				"degree" : "Bachelor of Science",
 				"majors" : "Computer Science in Media",
-				"dates" : "08/15/2009 - 08/15/2013",
+				"dates" : "2009 - 2013 ",
 				"url" : "www.hs-furtwangen.de"
 			}, 
+			{
+				"name" : "Concordia University",
+				"location" : "Montreal, Canada",
+				"degree" : "Semester Abroad",
+				"majors" : "Computer Science in Media",
+				"dates" : "2012",
+				"url" : "www.concordia.ca"
+			},
 			{
 				"name" : "Kaufmännische Schule Hechingen",
 				"location" : "Hechingen, Germany",
 				"degree" : "Higher Education Entrance Qualification",
 				"majors" : "Business Informatics",
-				"dates" : "08/15/2007 - 08/14/2009",
+				"dates" : "2007 - 2009 ",
 				"url" : "www.ks-hechingen.de"
 			}],
 	"onlineCourses" : [
@@ -43,8 +53,7 @@ var education = {
 		["school", "Udacity"],
 		["date", "February 2015"],
 		["url", "https://www.udacity.com/"]
-	],
-	"display" : "FUNCTION"
+	]
 };
 
 
@@ -52,31 +61,42 @@ var education = {
 var work = {
 	"jobs" : [{
 				"employer" : "Pentasys",
+				"title" : "Tester & Web Developer",
+				"location" : "Munich, Germany",
+				"date" : "since 2013",
+				"description" : loremIpsum
+			},
+			{
+				"employer" : "Pentasys",
 				"title" : "Bachelor's Degree Candidate",
 				"location" : "Munich, Germany",
-				"date" : "01/01/2013 - 06/30/2013",
-				"description" : "Writing of the bachelor thesis."
+				"date" : "2013",
+				"description" : loremIpsum
 			},
 			{
 				"employer" : "Squadhouse Media",
 				"title" : "Trainee",
 				"location" : "Tuttlingen, Germany",
-				"date" : "08/01/2010 - 02/28/2011",
-				"description" : "Agency for Media"
-			}],
-	"display" : "FUNCTION"
+				"date" : "2010 - 2011",
+				"description" : loremIpsum
+			}]
 };
 
 
 // PROJECTS
 var projects = {
 	"projects" : [{
-			"title" : "Horse Photography",
-			"dates" : "06/06/2013",
-			"description" : "Takeing Pictures of a Horse",
-			"images" : "images/fry.jpg"
-		}],
-	"display" : "FUNCTION"
+			"title" : "Austria Dog Photographer Meeting",
+			"dates" : "03/14/2015",
+			"description" : loremIpsum,
+			"images" : "images/832A8407_1.jpg"
+		},
+		{
+			"title" : "South German Horse Photographer Meeting",
+			"dates" : "02/11/2014",
+			"description" : loremIpsum,
+			"images" : "images/IMG_2026-2.jpg"
+		}]
 };
 
 
@@ -85,62 +105,67 @@ var projects = {
 //=============================================
 
 bio.display = function(){
-	var formattedBioName = HTMLheaderName.replace("%data%", bio.name);
-	var formattedBioRole = HTMLheaderRole.replace("%data%", bio.role);
+	var bioIdName = "#header";
+	var bioClassTopContactsName = "#topContacts";
+
+	var formattedBioName = HTMLheaderName.replace(replaceIdentifier, bio.name);
+	var formattedBioRole = HTMLheaderRole.replace(replaceIdentifier, bio.role);
 
 	var formattedBioHeadline = formattedBioName + formattedBioRole;
-	$("#header").prepend(formattedBioHeadline);
+	$(bioIdName).prepend(formattedBioHeadline);
 
-	$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-	$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
-	$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
-	$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
-	$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+	$(bioClassTopContactsName).append(HTMLmobile.replace(replaceIdentifier, bio.contacts.mobile));
+	$(bioClassTopContactsName).append(HTMLemail.replace(replaceIdentifier, bio.contacts.email));
+	$(bioClassTopContactsName).append(HTMLtwitter.replace(replaceIdentifier, bio.contacts.twitter));
+	$(bioClassTopContactsName).append(HTMLgithub.replace(replaceIdentifier, bio.contacts.github));
+	$(bioClassTopContactsName).append(HTMLlocation.replace(replaceIdentifier, bio.contacts.location));
 
-	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
-	$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+	$(bioIdName).append(HTMLwelcomeMsg.replace(replaceIdentifier, bio.welcomeMessage));
+	$(bioIdName).append(HTMLbioPic.replace(replaceIdentifier, bio.biopic));
 
-	$("#header").append(HTMLskillsStart);
+	$(bioIdName).append(HTMLskillsStart);
 	for(skill in bio.skills){
-		$("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
+		$("#skills").append(HTMLskills.replace(replaceIdentifier, bio.skills[skill]));
 	}	
 }
 
 
 
 work.display = function(){	
+	var workClassName = ".work-entry:last";
 	for(job in work.jobs){
 		$("#workExperience").append(HTMLworkStart);
-		$(".work-entry").append(HTMLworkEmployer.replace("%data%", work.jobs[job].employer));
-		$(".work-entry").append(HTMLworkTitle.replace("%data%", work.jobs[job].title));
-		$(".work-entry").append(HTMLworkLocation.replace("%data%", work.jobs[job].location));
-		$(".work-entry").append(HTMLworkDates.replace("%data%", work.jobs[job].date));
-		$(".work-entry").append(HTMLworkDescription.replace("%data%", work.jobs[job].description));
+		$(workClassName).append(HTMLworkEmployer.replace(replaceIdentifier, work.jobs[job].employer));
+		$(workClassName).append(HTMLworkTitle.replace(replaceIdentifier, work.jobs[job].title));
+		$(workClassName).append(HTMLworkLocation.replace(replaceIdentifier, work.jobs[job].location));
+		$(workClassName).append(HTMLworkDates.replace(replaceIdentifier, work.jobs[job].date));
+		$(workClassName).append(HTMLworkDescription.replace(replaceIdentifier, work.jobs[job].description));
 	}
 }
 
 
 
 projects.display = function(){
-	var projectClassName = ".project-entry";
+	var projectClassName = ".project-entry:last";
 	for(project in projects.projects){
 		$("#projects").append(HTMLprojectStart);
-		$(projectClassName).append(HTMLprojectTitle.replace("%data%", projects.projects[project].title));
-		$(projectClassName).append(HTMLprojectDates.replace("%data%", projects.projects[project].dates));
-		$(projectClassName).append(HTMLprojectDescription.replace("%data%", projects.projects[project].description));
-		$(projectClassName).append(HTMLprojectImage.replace("%data%", projects.projects[project].images));
+		$(projectClassName).append(HTMLprojectTitle.replace(replaceIdentifier, projects.projects[project].title));
+		$(projectClassName).append(HTMLprojectDates.replace(replaceIdentifier, projects.projects[project].dates));
+		$(projectClassName).append(HTMLprojectDescription.replace(replaceIdentifier, projects.projects[project].description));
+		$(projectClassName).append(HTMLprojectImage.replace(replaceIdentifier, projects.projects[project].images));
 	}		
 }
 
 
 education.display = function(){
+	var educationClassName = ".education-entry:last";
 	for(school in education.schools){
 		$("#education").append(HTMLschoolStart);
-		$(".education-entry").append(HTMLschoolName.replace("%data%", education.schools[school].name));
-		$(".education-entry").append(HTMLschoolDegree.replace("%data%", education.schools[school].degree));
-		$(".education-entry").append(HTMLschoolDates.replace("%data%", education.schools[school].dates));
-		$(".education-entry").append(HTMLschoolLocation.replace("%data%", education.schools[school].location));
-		$(".education-entry").append(HTMLschoolMajor.replace("%data%", education.schools[school].majors));
+		$(educationClassName).append(HTMLschoolName.replace(replaceIdentifier, education.schools[school].name));
+		$(educationClassName).append(HTMLschoolDegree.replace(replaceIdentifier, education.schools[school].degree));
+		$(educationClassName).append(HTMLschoolDates.replace(replaceIdentifier, education.schools[school].dates));
+		$(educationClassName).append(HTMLschoolLocation.replace(replaceIdentifier, education.schools[school].location));
+		$(educationClassName).append(HTMLschoolMajor.replace(replaceIdentifier, education.schools[school].majors));
 	}
 }
 
